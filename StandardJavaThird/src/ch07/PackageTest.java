d9a9fc1a1dba4295bf3b1efdd51d5a89c5b1a44c
zxcv5500 <zxcv5500@gmail.com>
2017-11-18 22:56:47 +0900

    패키지 컴파일 코드(패키지는 디렉토리라고 생각하면 된다)

---
+++ StandardJavaThird/src/ch07/PackageTest.java
@@ -0,0 +1,17 @@
+package ch07;
+
+/**
+ * @author zxcv5500
+ * 패키지 컴파일
+ * javac -d . PackageTest.java
+ * 
+ * 환경변수 등록
+ * .;C:\jdk1.8\work; 
+ */
+public class PackageTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
