339e858c6f73582a7d455be6dd2b9e92741f0ba7
zxcv5500 <zxcv5500@gmail.com>
2017-11-19 20:11:00 +0900

    instanceof를 사용해서 참조변수의 타입을 체크할 수 있다.

---
+++ StandardJavaThird/src/ch07/InstanceofTest.java
@@ -0,0 +1,25 @@
+package ch07;
+
+public class InstanceofTest {
+
+	public static void main(String[] args) {
+		FireEngine fe = new FireEngine();
+		
+		if (fe instanceof FireEngine) {
+			System.out.println("This is a FireEngine instance.");
+		}
+		
+		if (fe instanceof Car) {
+			System.out.println("This is a Car instance");
+		}
+		
+		if (fe instanceof Object) {
+			System.out.println("This is an Object instance.");
+		}
+		
+		System.out.println(fe.getClass().getName());					// 클래스의 이름을 출력
+		System.out.println(fe.getClass().getSimpleName());
+		
+	}
+
+}
