ae9fabe05ce0e7863654a051fba790a2e2bf042a
zxcv5500 <zxcv5500@gmail.com>
2017-11-19 17:57:22 +0900

    Time클래스의 멤버변수를 private로 선언하고 public getter/setter() 메소드 코드 작성.

---
+++ StandardJavaThird/src/ch07/TimeTest.java
@@ -0,0 +1,59 @@
+package ch07;
+
+public class TimeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Time t = new Time(12, 35, 30);
+		System.out.println(t);
+//		t.hour = 13;							// 에러! 변수 hour의 접근제어자가 private이므로 접근할 수 없다.
+		t.setHour(t.getHour()+1);				// 현재시간보다 1시간 후로 변경한다.
+		System.out.println(t);					// System.out.println(t.toString());과 같다.
+
+	}
+
+}
+
+class Time {
+	private int hour;
+	private int minute;
+	private int second;
+	
+	Time(int hour, int minute, int second) {
+		setHour(hour);
+		setMinute(minute);
+		setSecond(second);
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+	
+	public void setHour(int hour) {
+		if (hour < 0 || hour > 23) return;
+		this.hour = hour;
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+	
+	public void setMinute(int minute) {
+		if (minute < 0 || minute > 59) return;
+		this.minute = minute;
+	}
+	
+	public int getSecond() {
+		return second;
+	}
+	
+	public void setSecond(int second) {
+		if (second > 0 || second < 59) return;
+		this.second = second;
+	}
+	
+	public String toString() {
+		return hour + ":" + minute + ":" + second;
+	}
+	
+}
