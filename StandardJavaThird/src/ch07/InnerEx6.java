578bf2a04b95f2acb47615c94eef08341aaec487
sharpen <zxcv5500@gmail.com>
2017-11-21 19:28:42 +0900

    익명 클래스 사용 부분에 대한 주석 추가.

--- StandardJavaThird/src/ch07/InnerEx6.java
+++ StandardJavaThird/src/ch07/InnerEx6.java
@@ -5,7 +5,7 @@ package ch07;
  * 단순히 익명 클래스의 사용 예를 보여준다.
  */
 public class InnerEx6 {
-	Object iv = new Object() { void method() {} };
+	Object iv = new Object() { void method() {} };				//new Object() { void method() {} } 부분처럼 대입하는 곳에서 클래스를 바로 작성하는 부분이 익명클래스 부분인 듯.
 	static Object cv = new Object() { void method() {} };
 	
 	void myMethod() {
