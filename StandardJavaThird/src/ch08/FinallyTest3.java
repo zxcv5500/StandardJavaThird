d3f88467f9a66c2fa3140522c6d59875cce57d64
zxcv5500 <zxcv5500@gmail.com>
2017-11-22 03:39:26 +0900

    try나 catch문에서 메소드를 종료하라는 return문을 만나더라도 finally문의 처리를 다 하고 return;이 실행된다.

---
+++ StandardJavaThird/src/ch08/FinallyTest3.java
@@ -0,0 +1,27 @@
+package ch08;
+
+/**
+ * @author zxcv5500
+ * try나 catch문에서 메소드를 종료하라는 return문을 만나더라도
+ * finally문의 처리를 다 하고 return;이 실행된다.
+ */
+class FinallyTest3 {
+
+	public static void main(String[] args) {
+		// method1()은 static메서드 이므로 인스턴스 생성없이 직접 호출이 가능하다.
+		FinallyTest3.method1();
+		System.out.println("method1()의 수행을 마치고 main메서드로 돌아왔습니다.");
+	}
+	
+	static void method1() {
+		try {
+			System.out.println("method1()이 호출 되었습니다.");
+			return;				// 현재 실행 중인 메서드를 종료한다.
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("method1()의 finally블럭이 실행되었습니다.");
+		}
+	}
+
+}
