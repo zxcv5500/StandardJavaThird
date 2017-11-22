30ddfc3dce2457513589106f58f0a70cbdde8f6f
zxcv5500 <zxcv5500@gmail.com>
2017-11-23 03:09:50 +0900

    문자열 리터럴

---
+++ StandardJavaThird/src/ch09/StringEx2.java
@@ -0,0 +1,16 @@
+package ch09;
+
+/**
+ * @author zxcv5500
+ * 문자열 리터럴
+ */
+class StringEx2 {
+
+	public static void main(String[] args) {
+		String s1 = "AAA";
+		String s2 = "AAA";
+		String s3 = "AAA";
+		String s4 = "BBB";
+	}
+
+}
