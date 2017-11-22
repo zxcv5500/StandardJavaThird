cb3f6d014ee5111ecb5d45c8bf03acddf2ae1e96
sharpen <zxcv5500@gmail.com>
2017-11-22 20:11:17 +0900

    [예제 8-23] 연결된 예외 예제.

---
+++ StandardJavaThird/src/ch08/ChainedExceptionEx.java
@@ -0,0 +1,85 @@
+package ch08;
+
+/**
+ * @author sharpen
+ * [예제 8-23] 연결된 예외 예제.
+ */
+class ChainedExceptionEx {
+
+	public static void main(String[] args) {
+		try {
+			install();
+		} catch (InstallException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void install() throws InstallException {
+		try {
+			startInstall();			//프로그램 설치에 필요한 준비를 한다.
+			copyFiles();
+		} catch (SpaceException e) {
+			InstallException ie = new InstallException("설치 중 예외 발생");
+			ie.initCause(e);
+			throw ie;
+		} catch (MemoryException me) {
+			InstallException ie = new InstallException("설치 중 예외 발생");
+			ie.initCause(me);
+			throw ie;
+		} finally {
+			deleteTempFiles();
+		}
+	}
+	
+	static void startInstall() throws SpaceException, MemoryException {
+		if (!enoughSpace()) {		// 충분한 설치공간이 없으면
+			throw new SpaceException("설치할 공간이 부족합니다.");
+		}
+		
+		if (!enoughMemory()) {
+			throw new MemoryException("메모리가 부족합니다.");
+//			throw new RuntimeException(new MemoryException("메모리가 부족합니다."));
+			
+		}
+	}
+	
+	static void copyFiles() {
+		/* 파일들을 복사하는 코드를 적는다. */
+	}
+	
+	static void deleteTempFiles() {
+		/* 임시설치파일들을 삭제하는 코드를 적는다. */		
+	}
+	
+	static boolean enoughSpace() {
+		// 설치하는데 필요한 공간이 있는지 확인하는 코드를 적는다.
+		return false;
+	}
+	
+	static boolean enoughMemory() {
+		// 설치하는데 필요한 메모리 공간이 있는지 확인하는 코드를 적는다.
+		return true;
+	}
+
+}
+
+
+class InstallException extends Exception {
+	InstallException(String msg) {
+		super(msg);
+	}
+}
+
+class SpaceException extends Exception {
+	SpaceException(String msg) {
+		super(msg);
+	}
+}
+
+class MemoryException extends Exception {
+	MemoryException(String msg) {
+		super(msg);
+	}
+}
