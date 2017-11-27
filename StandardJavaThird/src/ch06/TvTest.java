f18315af547fe5fa7378db9713f59541120e5137
zxcv5500 <zxcv5500@gmail.com>
2017-11-28 01:32:31 +0900

    초기화를 한줄에 하는 코드를 달고 주석처리함.

--- StandardJavaThird/src/ch06/TvTest.java
+++ StandardJavaThird/src/ch06/TvTest.java
@@ -25,6 +25,8 @@ public class TvTest {
 	public static void main(String[] args) {
 		Tv t;					// Tv인스턴스를 참조하기 위한 변수 t를 선언
 		t = new Tv();			// Tv인스턴스를 생성한다.
+		// Tv t = new Tv();		/* 위의 두줄을 한줄로 이렇게 표현 할 수 있다.*/
+		
 		t.channel = 7;			// Tv인스턴스의 멤버변수 channel의 값을 7로 한다.
 		t.channelDown();		// Tv인스턴스의 메서드 channelDown()을 호출한다.
 		System.out.println("현재 채널은 " + t.channel + "입니다.");
