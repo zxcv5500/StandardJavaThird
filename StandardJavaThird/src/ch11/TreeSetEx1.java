c786a5cbc65486a03700cc3746d6f51bba7f355a
zxcv5500 <zxcv5500@gmail.com>
2017-12-10 05:03:53 +0900

    subSet()을 이용한 범위 검색 예제.

---
+++ StandardJavaThird/src/ch11/TreeSetEx1.java
@@ -0,0 +1,29 @@
+package ch11;
+
+import java.util.TreeSet;
+
+/**
+ * @author zxcv5500
+ * subSet()을 이용한 범위 검색 예제.
+ */
+class TreeSetEx1 {
+
+	public static void main(String[] args) {
+		TreeSet set = new TreeSet();
+		
+		String from = "b";
+		String to = "d";
+		
+		set.add("abc"); set.add("alien"); set.add("bat");
+		set.add("car"); set.add("Car"); set.add("disc");
+		set.add("dance"); set.add("dZZZZ"); set.add("dzzzz");
+		set.add("elephant"); set.add("elevator"); set.add("fan");
+		set.add("flower");
+		
+		System.out.println(set);
+		System.out.println("range search : from " + from + " to " + to);
+		System.out.println("result1 : " + set.subSet(from, to));
+		System.out.println("result2 : " + set.subSet(from, to + "zzz"));
+	}
+
+}
