88fd77e3557633a6f8781743b9e7149eec4d8899
zxcv5500 <zxcv5500@gmail.com>
2017-12-10 05:12:43 +0900

    공백 (' ')이후의 문자들을 출력하는 예제.

---
+++ StandardJavaThird/src/ch11/AsciiPrint.java
@@ -0,0 +1,16 @@
+package ch11;
+
+/**
+ * @author zxcv5500
+ * 공백 (' ')이후의 문자들을 출력하는 예제.
+ */
+class AsciiPrint {
+
+	public static void main(String[] args) {
+		char ch = ' ';							// 공백 (' ')이후의 문자들을 출력한다.
+		for (int i=0; i < 95; i++) {
+			System.out.print(ch++);
+		}
+	}
+
+}
