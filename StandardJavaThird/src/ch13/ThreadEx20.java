a7d9028f0318ed76da2c8ac0ca9c112596ce91ce
sharpen <zxcv5500@gmail.com>
2017-12-19 17:13:05 +0900

    jvm을 흉내내본 예제.

---
+++ StandardJavaThird/src/ch13/ThreadEx20.java
@@ -0,0 +1,63 @@
+package ch13;
+
+class ThreadEx20_01 extends Thread {
+	final static int MAX_MEMORY = 1000;
+	int usedMemory = 0;
+	
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(10 * 1000);			// 10초를 기다린다.
+			} catch (InterruptedException e) {
+				System.out.println("Awaken by interrupt()");
+			}
+		}
+	}
+	
+	public void gc() {
+		usedMemory -= 300;
+		if (usedMemory < 0) {
+			usedMemory = 0;
+		}
+	}
+	
+	public int totalMemory() {
+		return MAX_MEMORY;
+	}
+	
+	public int freeMemory() {
+		return MAX_MEMORY - usedMemory;
+	}
+	
+	
+}
+
+/**
+ * @author sharpen
+ * jvm을 흉내내본 예제.
+ */
+class ThreadEx20 {
+
+	public static void main(String[] args) {
+		ThreadEx20_01 gc = new ThreadEx20_01();
+		gc.setDaemon(true);
+		gc.start();
+		
+		int requiredMemory = 0;
+		
+		for(int i=0; i <20; i++) {
+			requiredMemory = (int) (Math.random() * 10) * 20;
+			
+			// 필요한 메모리가 사용할 수 있는 양보다 크거나 전체 메모리의 60% 이상을
+			// 사용했을 경우 gc를 깨운다.
+			if (gc.freeMemory() < requiredMemory || gc.freeMemory() < gc.totalMemory() * 0.4) {
+				gc.interrupt();			// 잠자고 있는 쓰레드 gc를 깨운다.
+			}
+			
+			gc.usedMemory += requiredMemory;
+			System.out.println("usedMemory:" + gc.usedMemory);
+		}
+	}
+
+}
