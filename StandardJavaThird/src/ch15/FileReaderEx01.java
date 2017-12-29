98d0fb398819a97ef9a5b8424da63c9711cee904
zxcv5500 <zxcv5500@gmail.com>
2017-12-29 14:29:11 +0900

    파일을 읽어서 화면에 출력하는 예제.

---
+++ StandardJavaThird/src/ch15/FileReaderEx01.java
@@ -0,0 +1,39 @@
+package ch15;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author zxcv5500
+ * 파일을 읽어서 화면에 출력하는 예제.
+ */
+class FileReaderEx01 {
+
+	public static void main(String[] args) {
+		try {
+			String fileName = "test.txt";
+			FileInputStream fis = new FileInputStream(fileName);
+			FileReader fr = new FileReader(fileName);
+			
+			int data = 0;
+			// FileInputStream을 이용해서 파일내용을 읽어 화면에 출력한다.
+			while ((data=fis.read()) != -1) {
+				System.out.print((char) data);
+			}
+			System.out.println();
+			fis.close();
+			
+			// FileReader를 이용해서 파일 내용을 읽어 화면에 출력한다.
+			while ((data = fr.read()) != -1) {
+				System.out.print((char) data);
+			}
+			System.out.println();
+			fr.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
