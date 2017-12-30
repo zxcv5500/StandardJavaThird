ce55ffa625ed37d72cb01c925e1f1fc992e0e0ab
sharpen <zxcv5500@gmail.com>
2017-12-30 16:43:35 +0900

    표준 입출력의 대상변경

---
+++ StandardJavaThird/src/ch15/StandardIOEx02.java
@@ -0,0 +1,14 @@
+package ch15;
+
+/**
+ * @author sharpen
+ * 표준 입출력의 대상변경
+ */
+class StandardIOEx02 {
+
+	public static void main(String[] args) {
+		System.out.println("out : Hello World!");
+		System.err.println("err : Hello World!");
+	}
+
+}
