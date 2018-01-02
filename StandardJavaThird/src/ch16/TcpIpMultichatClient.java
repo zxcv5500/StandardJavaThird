f0e87a962481a805ac4d05c7d73d6d486f737d46
sharpen <zxcv5500@gmail.com>
2018-01-02 21:01:52 +0900

    여러 클라이언트가 서버에 접속해서 채팅을 할 수 있는 멀티채팅서버 프로그램을 작성함

---
+++ StandardJavaThird/src/ch16/TcpIpMultichatClient.java
@@ -0,0 +1,87 @@
+package ch16;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @author sharpen
+ * 여러 클라이언트가 서버에 접속해서 채팅을 할 수 있는 멀티채팅서버 프로그램을 작성함
+ */
+public class TcpIpMultichatClient {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("USAGE : java TcpIpMultichatClient 대화명");
+			System.exit(0);
+		}
+		
+		try {
+			String serverIp = "127.0.0.1";
+			// 소켓을 생성하여 연결을 요청한다.
+			Socket socket = new Socket(serverIp, 7777);
+			System.out.println("서버에 연결되었습니다.");
+			Thread sender = new Thread(new ClientSender(socket, args[0]));
+			Thread receiver = new Thread(new ClientReceiver(socket));
+			
+			sender.start();
+			receiver.start();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+	
+	static class ClientSender extends Thread {
+		Socket socket;
+		DataOutputStream out;
+		String name;
+		
+		ClientSender(Socket socket, String name) {
+			this.socket = socket;
+			try {
+				out = new DataOutputStream(socket.getOutputStream());
+				this.name = name;
+			} catch (Exception e) {			}
+		}
+		
+		public void run() {
+			Scanner scanner = new Scanner(System.in);
+			try {
+				if (out != null) {
+					out.writeUTF(name);
+				}
+				
+				while (out != null) {
+					out.writeUTF("[" + name + "]" + scanner.nextLine());
+				}
+			} catch (Exception e) {			}
+		}
+	}
+	
+	static class ClientReceiver extends Thread {
+		Socket socket;
+		DataInputStream in;
+		
+		public ClientReceiver(Socket socket) {
+			this.socket = socket;
+			try {
+				in = new DataInputStream(socket.getInputStream());
+			} catch (IOException e) {			}
+		}
+		
+		public void run() {
+			while(in != null) {
+				try {
+					System.out.println(in.readUTF());
+				} catch (IOException e) {
+					// TODO: handle exception
+				}
+			}
+		}
+	}
+
+}
