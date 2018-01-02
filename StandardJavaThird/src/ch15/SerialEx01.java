4d964178929f87c5b87f7ed6e4630a89743b7621
zxcv5500 <zxcv5500@gmail.com>
2018-01-02 12:55:57 +0900

    생성한 객체를 직렬화하여 파일(UserInfo.ser)에 저장하는 예제

---
+++ StandardJavaThird/src/ch15/SerialEx01.java
@@ -0,0 +1,43 @@
+package ch15;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ * @author zxcv5500
+ * 생성한 객체를 직렬화하여 파일(UserInfo.ser)에 저장하는 예제
+ */
+class SerialEx01 {
+
+	public static void main(String[] args) {
+		try {
+			String fileName = "UserInfo.ser";
+			FileOutputStream fos = new FileOutputStream(fileName);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			
+			ObjectOutputStream out = new ObjectOutputStream(bos);
+			
+			UserInfo u1 = new UserInfo("JavaMan", "1234", 30);
+			UserInfo u2 = new UserInfo("JavaWoman", "4321", 26);
+			
+			ArrayList<UserInfo> list = new ArrayList<>();
+			list.add(u1);
+			list.add(u2);
+			
+			// 객체를 직렬화 한다.
+			out.writeObject(u1);
+			out.writeObject(u2);
+			out.writeObject(list);
+			
+			out.close();
+			
+			System.out.println("직렬화가 잘 끝났습니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
